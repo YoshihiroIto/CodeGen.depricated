ad8c031e383506bcde785ae4fa17d159430ca034
Yoshihiro Ito <yo.i.jewelry.bab@gmail.com>
2019-02-11 11:10:26 +0900

    First commit

---
+++ CodeGen/Config.cs
@@ -0,0 +1,29 @@
+﻿namespace CodeGen
+{
+    public class Config
+    {
+        public string[] Keywords { get; set; }
+
+        public Process[] Processes { get; set; }
+    }
+
+    public class Process
+    {
+        public ProcessKind Kind { get; set; }
+
+        // Template
+        public string TemplateFile { get; set; }
+        public string OutputFile { get; set; }
+
+        // Patch
+        public string TargetFile { get; set; }
+        public string Tag { get; set; }
+        public string Input { get; set; }
+    }
+
+    public enum ProcessKind
+    {
+        Template,
+        Patch
+    }
+}
---
+++ CodeGen/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.IO;
+
+namespace CodeGen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var configFilePath = args[0];
+
+            var configDirPath = Path.GetDirectoryName(configFilePath);
+            var config = Utf8Json.JsonSerializer.Deserialize<Config>(File.ReadAllBytes(configFilePath));
+
+            if (string.IsNullOrEmpty(configDirPath) == false)
+                Directory.SetCurrentDirectory(configDirPath);
+
+            foreach (var keyword in config.Keywords)
+            {
+                Console.WriteLine($"Input {keyword}.");
+
+                Environment.SetEnvironmentVariable(keyword, Console.ReadLine());
+            }
+
+            foreach (var p in config.Processes)
+            {
+                switch (p.Kind)
+                {
+                    case ProcessKind.Template:
+
+                        var template = Environment.ExpandEnvironmentVariables(File.ReadAllText(p.TemplateFile));
+
+                        var outputPath = Environment.ExpandEnvironmentVariables(p.OutputFile);
+                        var outputDir = Path.GetDirectoryName(outputPath);
+
+                        if (string.IsNullOrEmpty(outputDir) == false)
+                            Directory.CreateDirectory(outputDir);
+
+                        File.WriteAllText(outputPath, template);
+
+                        break;
+
+                    case ProcessKind.Patch:
+                        var targetFile = Environment.ExpandEnvironmentVariables(p.TargetFile);
+                        var targetFileText = File.ReadAllText(targetFile);
+
+                        var input = Environment.ExpandEnvironmentVariables(p.Input);
+
+                        targetFileText = targetFileText.Replace(p.Tag, input);
+
+                        File.WriteAllText(targetFile, targetFileText);
+
+                        break;
+
+                    default:
+                        throw new ArgumentOutOfRangeException();
+                }
+            }
+        }
+    }
+}
